2b0e8960a9633d3c8d4cc94f8ef94651826c66b8
liviu-gc <71663921+liviu-gc@users.noreply.github.com>
2020-09-21 22:48:38 +0300

    Create wait-for.js

---
+++ wait-for.js
@@ -0,0 +1,70 @@
+var waitFor = (function(){
+  
+  var id = 0;
+  
+  return function (p1, p2, p3) {
+    
+    if (!new.target) { throw 'waitFor() must be called with new'; }
+    
+    var t = this, _t = {
+      id: ++id,
+      T: null,
+      iti: 250,
+      fnCondition:null,
+      fnCallback:null,
+      cancelable: true,
+      stop: false
+    };
+    
+    if (typeof p1 === 'undefined')
+      return false;
+    
+    if (typeof p1 === 'function' && typeof p2 === 'function') {
+      
+      _t.fnCondition = p1;
+      _t.fnCallback = p2;
+      
+      if (typeof p3 === 'number')
+        _t.iti = p3;
+        
+    } else if (typeof p1 === 'object' && p1.cond && p1.call) {
+      
+      _t.fnCondition = p1.cond;
+      _t.fnCallback = p1.call;
+      
+      if (typeof p1.iti === 'number')
+        _t.iti = p1.iti;
+      
+      if (typeof p1.cancelable !== 'undefined')
+        _t.cancelable = Boolean(p1.cancelable);
+     
+    }
+    
+    if (!_t.fnCondition || !_t.fnCallback)
+      return false;
+    
+    _t.T = setInterval( function() {
+      if (_t.fnCondition( t.getId() )) {
+        clearInterval( _t.T );
+        if (t._stop)
+          return false;
+        _t.fnCallback( t.getId() );
+      }
+    }, _t.iti);
+    
+    t.getId = function() {
+      return _t.id;
+    }
+    
+    t.cancel = function(){
+      if (!_t.cancelable)
+        return false;
+      
+      _t.stop = true;
+      clearInterval( _t.T );
+    }
+    
+    return t;
+  };
+
+})();
